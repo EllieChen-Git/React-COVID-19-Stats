ab93410cd2483db42c1633537fad9f0cf9704c74
Ellie <elliechen.etc@gmail.com>
2020-04-15 12:17:21 +1000

    Clock: Fixed ticking issue

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,55 +1,37 @@
 import React, { Component } from "react";
-import "./Clock.css";
 
 class Clock extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: 1
-    };
-  }
-
-  tick = () => {
-    this.setState({ time: this.state.time + 1 });
-  };
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
   render() {
     const { minutes, hours, seconds, timezone, icon } = this.props;
 
     return (
-      <div className="container" onLoad={this.tick}>
-        <h3 className="label">{timezone}</h3>
-        <p className="label">
+      <div className="clock">
+        <h3 className="clock-label">{timezone}</h3>
+        <p className="clock-label">
           {hours}:{minutes}:{seconds}
         </p>
 
         <div
-          className="clock-face"
+          className="clock-face-container"
           style={{ backgroundImage: `url(/${icon})` }}
         >
-          <div className="clock">
-            <div className="hours-container">
+          <div className="clock-face">
+            <div className="clock-hours-container">
               <div
-                className="hours"
-                style={{ transform: `rotateZ(${hours * 30 + minutes / 2}deg)` }}
+                className="clock-hours"
+                style={{
+                  transform: `rotateZ(${hours * 30 + minutes / 2}deg)`,
+                }}
               ></div>
             </div>
-            <div className="minutes-container">
+            <div className="clock-minutes-container">
               <div
-                className="minutes"
+                className="clock-minutes"
                 style={{ transform: `rotateZ(${minutes * 6}deg)` }}
               ></div>
-              <div className="seconds-container">
+              <div className="clock-seconds-container">
                 <div
-                  className="seconds"
+                  className="clock-seconds"
                   style={{ transform: `rotateZ(${seconds * 6}deg)` }}
                 ></div>
               </div>
